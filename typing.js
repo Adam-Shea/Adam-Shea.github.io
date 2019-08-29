615a47228a92fef0834da3d4ebc9970257435582
Adam Shea <44814104+Adam-Shea@users.noreply.github.com>
2019-08-29 20:02:52 +0100

    ReWrote CSS and HTML to be more efficent

---
+++ typing.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = "Hi, I'm Adam Shea"; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("typeText").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+typeWriter();
